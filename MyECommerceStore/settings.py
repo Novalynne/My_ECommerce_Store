76ca7ee7a489cbdd260b2123cecd7b83d6aabf7c
Novalynne <schirone.ma@gmail.com>
2025-06-05 00:52:06 +0200

    fixed footer in base.html added homepage and base_css

--- MyECommerceStore/settings.py
+++ MyECommerceStore/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/css'), #TODO: Add your CSS directory
-                    os.path.join(BASE_DIR, 'static/javascript')] #TODO: Add your JavaScript directory
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/css')]
+                    #os.path.join(BASE_DIR, 'static/javascript')] TODO: Add your JavaScript directory
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')         # Directory for images files, now that i save images in cloudinary this is not needed
 
